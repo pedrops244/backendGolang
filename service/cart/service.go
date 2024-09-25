5618b58a27242e62dcf117613d1b09cfb9d6c575
Pedro Souza <pedrops244@hotmail.com>
2024-09-25 15:46:22 -0300

    little thinks changed

--- service/cart/service.go
+++ service/cart/service.go
@@ -6,82 +6,91 @@ import (
 	"github.com/sikozonpc/ecom/types"
 )
 
-func getCartItemsIDs(items []types.CartItem) ([]int, error) {
+func getCartItemsIDs(items []types.CartCheckoutItem) ([]int, error) {
 	productIds := make([]int, len(items))
 	for i, item := range items {
 		if item.Quantity <= 0 {
-			return nil, fmt.Errorf("invalid quantity for the product %d", item.ProductID)
+			return nil, fmt.Errorf("invalid quantity for product %d", item.ProductID)
 		}
+
 		productIds[i] = item.ProductID
 	}
+
 	return productIds, nil
 }
 
-func (h *Handler) createOrder(ps []types.Product, items []types.CartItem, userID int) (int, float64, error) {
-	productMap := make(map[int]types.Product)
-	for _, product := range ps {
-		productMap[product.ID] = product
-	}
-	// check if all products are actually in stock
-	if err := checkIfCartIsInStock(items, productMap); err != nil {
-		return 0, 0, nil
-	}
-	// calculate the total price
-	totalPrice := calculateTotalPrice(items, productMap)
-	// reduce quantity of products in our db
-	for _, item := range items {
-		product := productMap[item.ProductID]
-		product.Quantity -= item.Quantity
-
-		h.productStore.UpdateProduct(product)
-	}
-
-	// create the order
-	orderID, err := h.store.CreateOrder(types.Order{
-		UserID:  userID,
-		Total:   totalPrice,
-		Status:  "pending",
-		Address: "some address",
-	})
-
-	// create order items
-	for _, item := range items {
-		h.store.CreateOrderItem(types.OrderItem{
-			OrderID:   orderID,
-			ProductID: item.ProductID,
-			Quantity:  item.Quantity,
-			Price:     productMap[item.ProductID].Price,
-		})
-		if err != nil {
-			return 0, 0, err
-		}
-	}
-
-	return orderID, totalPrice, nil
-}
-
-func checkIfCartIsInStock(cartItems []types.CartItem, products map[int]types.Product) error {
+func checkIfCartIsInStock(cartItems []types.CartCheckoutItem, products map[int]types.Product) error {
 	if len(cartItems) == 0 {
 		return fmt.Errorf("cart is empty")
 	}
+
 	for _, item := range cartItems {
 		product, ok := products[item.ProductID]
 		if !ok {
 			return fmt.Errorf("product %d is not available in the store, please refresh your cart", item.ProductID)
 		}
+
 		if product.Quantity < item.Quantity {
 			return fmt.Errorf("product %s is not available in the quantity requested", product.Name)
 		}
 	}
+
 	return nil
 }
 
-func calculateTotalPrice(cartItems []types.CartItem, products map[int]types.Product) float64 {
+func calculateTotalPrice(cartItems []types.CartCheckoutItem, products map[int]types.Product) float64 {
 	var total float64
 
 	for _, item := range cartItems {
 		product := products[item.ProductID]
 		total += product.Price * float64(item.Quantity)
 	}
+
 	return total
+}
+
+func (h *Handler) createOrder(products []types.Product, cartItems []types.CartCheckoutItem, userID int) (int, float64, error) {
+	// create a map of products for easier access
+	productsMap := make(map[int]types.Product)
+	for _, product := range products {
+		productsMap[product.ID] = product
+	}
+
+	// check if all products are available
+	if err := checkIfCartIsInStock(cartItems, productsMap); err != nil {
+		return 0, 0, err
+	}
+
+	// calculate total price
+	totalPrice := calculateTotalPrice(cartItems, productsMap)
+
+	// reduce the quantity of products in the store
+	for _, item := range cartItems {
+		product := productsMap[item.ProductID]
+		product.Quantity -= item.Quantity
+		h.store.UpdateProduct(product)
+	}
+
+	// create order record
+	orderID, err := h.orderStore.CreateOrder(types.Order{
+		UserID:  userID,
+		Total:   totalPrice,
+		Status:  "pending",
+		Address: "some address", // could fetch address from a user addresses table
+	})
+	if err != nil {
+		return 0, 0, err
+	}
+
+	// create order the items records
+	for _, item := range cartItems {
+		h.orderStore.CreateOrderItem(types.OrderItem{
+			OrderID:   orderID,
+			ProductID: item.ProductID,
+			Quantity:  item.Quantity,
+			Price:     productsMap[item.ProductID].Price,
+		})
+	}
+
+	return orderID, totalPrice, nil
 }
